8640acd4765da734ba555c83d8afb30b3c050701
Deepak Yadav <deepak@appnexio.com>
2022-07-09 08:25:01 +0530

    add instructions of status

--- git.php
+++ git.php
@@ -1,2 +1,18 @@
- git clone https://github.com/dpkphp/training.git
- 
+<?php
+ // 1. download the repository
+ // git clone https://github.com/dpkphp/training.git
+
+ // step 2
+ // steps to commit and push the files to git repository
+ // git add .
+
+ // step 3
+// git commit -m "type your message"
+
+// step 4
+// git push
+
+// see the changes are reflected in your repository
+
+
+// git status (check if any files or folders are changed)
