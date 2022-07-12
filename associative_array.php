b25750c403bd2306c5374a74c180a179e7ff92ef
Deepak Yadav <deepak@appnexio.com>
2022-07-12 09:11:19 +0530

    create session and cookies

--- associative_array.php
+++ associative_array.php
@@ -1,6 +1,6 @@
 <?php 
 $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43"); // associative array
-
+$age ["Deepak"] = 40;
 echo "Peter is " . $age['Peter'] . " years old. <br> \n"; // array key
 
 // iterating array using foreach loop
